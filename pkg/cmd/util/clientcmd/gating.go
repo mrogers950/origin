5dd556c499913a25795bb3f475849f0e24be557c
Matt Rogers <mrogers@redhat.com>
2017-09-12 17:23:57 -0400

    WIP change tests

--- pkg/cmd/util/clientcmd/gating.go
+++ pkg/cmd/util/clientcmd/gating.go
@@ -5,33 +5,58 @@ import (
 
 	"github.com/openshift/origin/pkg/authorization/apis/authorization"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	kclientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	"k8s.io/client-go/discovery"
 )
 
 // LegacyPolicyResourceGate returns err if the server does not support any of the legacy policy objects (< 3.7)
-func LegacyPolicyResourceGate(client kclientset.Interface) error {
-	legacyResources := []string{"clusterpolicies", "clusterpolicybindings", "policies", "policybindings"}
-	schemeResources := []schema.GroupVersionResource{}
-	for _, resource := range legacyResources {
-		// We want to check legacy and current group versions for each resource
-		schemeResources = append(schemeResources, schema.GroupVersionResource{
+func LegacyPolicyResourceGate(client discovery.DiscoveryInterface) error {
+	res, err := DiscoverGroupVersionResources(client,
+		schema.GroupVersionResource{
 			Group:    authorization.LegacySchemeGroupVersion.Group,
 			Version:  authorization.LegacySchemeGroupVersion.Version,
-			Resource: resource,
-		})
-		schemeResources = append(schemeResources, schema.GroupVersionResource{
+			Resource: "clusterpolicies",
+		},
+		schema.GroupVersionResource{
+			Group:    authorization.LegacySchemeGroupVersion.Group,
+			Version:  authorization.LegacySchemeGroupVersion.Version,
+			Resource: "clusterpolicybindings",
+		},
+		schema.GroupVersionResource{
+			Group:    authorization.LegacySchemeGroupVersion.Group,
+			Version:  authorization.LegacySchemeGroupVersion.Version,
+			Resource: "policies",
+		},
+		schema.GroupVersionResource{
+			Group:    authorization.LegacySchemeGroupVersion.Group,
+			Version:  authorization.LegacySchemeGroupVersion.Version,
+			Resource: "policybindings",
+		},
+		schema.GroupVersionResource{
 			Group:    authorization.SchemeGroupVersion.Group,
 			Version:  authorization.SchemeGroupVersion.Version,
-			Resource: resource,
+			Resource: "clusterpolicies",
+		},
+		schema.GroupVersionResource{
+			Group:    authorization.SchemeGroupVersion.Group,
+			Version:  authorization.SchemeGroupVersion.Version,
+			Resource: "clusterpolicybindings",
+		},
+		schema.GroupVersionResource{
+			Group:    authorization.SchemeGroupVersion.Group,
+			Version:  authorization.SchemeGroupVersion.Version,
+			Resource: "policies",
+		},
+		schema.GroupVersionResource{
+			Group:    authorization.SchemeGroupVersion.Group,
+			Version:  authorization.SchemeGroupVersion.Version,
+			Resource: "policybindings",
 		})
-	}
 
-	res, err := DiscoverGroupVersionResources(client, schemeResources)
 	if err != nil {
 		return err
 	}
 
-	if len(res) != len(legacyResources) {
+	if len(res) != 4 {
 		return fmt.Errorf("the server does not support legacy policy resources")
 	}
 
@@ -39,36 +64,26 @@ func LegacyPolicyResourceGate(client kclientset.Interface) error {
 }
 
 // DiscoverGroupVersionResources performs a server resource discovery for each GroupVersionResource in
-// filterGVR, returning a slice of GroupVersionResources containing the discovered resources. If a filterGVR
-// element has a non-empty Resource, then only the matching resource is added to the returning slice. A
-// filterGVR element with an empty Resource returns all resources discovered for the group and version.
-func DiscoverGroupVersionResources(client kclientset.Interface, filterGVR []schema.GroupVersionResource) ([]schema.GroupVersionResource, error) {
+// filterGVR, returning a slice of GroupVersionResources containing the discovered resources.
+func DiscoverGroupVersionResources(client discovery.ServerResourcesInterface, filterGVR ...schema.GroupVersionResource) ([]schema.GroupVersionResource, error) {
 	if len(filterGVR) == 0 {
 		return nil, fmt.Errorf("at least one GroupVersionResource must be provided")
 	}
 
 	ret := []schema.GroupVersionResource{}
-	for _, filter := range filterGVR {
+	for i := range filterGVR {
 		// Discover the list of resources for this GVR
-		serverResources, err := client.Discovery().ServerResourcesForGroupVersion(filter.String())
+		gv := filterGVR[i].GroupVersion()
+		serverResources, err := client.ServerResourcesForGroupVersion(gv.String())
 		if err != nil {
-			continue
+			return nil, err
 		}
 
 		for _, resource := range serverResources.APIResources {
 			// If a resource name was given, return the matching GVR
-			if len(filter.Resource) > 0 {
-				if filter.Resource == resource.Name {
-					ret = append(ret, filter)
-					break
-				}
-			} else { // No resource, return a GVR for each resource
-				new := schema.GroupVersionResource{
-					Group:    filter.Group,
-					Version:  filter.Version,
-					Resource: resource.Name,
-				}
-				ret = append(ret, new)
+			if filterGVR[i].Resource == resource.Name {
+				ret = append(ret, filterGVR[i])
+				break
 			}
 		}
 	}
--- pkg/cmd/util/clientcmd/gating_test.go
+++ pkg/cmd/util/clientcmd/gating_test.go
@@ -1,223 +1,167 @@
 package clientcmd
 
 import (
-	"reflect"
-	"testing"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	fake "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/fake"
-)
-
-func TestResourceGate(t *testing.T) {
-	legacyTests := map[string]struct {
-		//existingAPIResources []*metav1.APIResourceList
-		//inputGVR             []runtime.Object
-		existingAPIResources []runtime.Object
-		expectedErr          string
-	}{
-		"legacy-policy-all-supported": {
-			existingAPIResources: []runtime.Object{
-				&metav1.APIResourceList{
-					GroupVersion: "a/v1",
-					APIResources: []metav1.APIResource{
-						{
-							Name: "clusterpolicies",
-						},
-						{
-							Name: "policybindings",
-						},
-						{
-							Name: "policies",
-						},
-						{
-							Name: "clusterpolicybindings",
-						},
-						{
-							Name: "foo1",
-						},
-						{
-							Name: "foo2",
-						},
-					},
-				},
-				&metav1.APIResourceList{
-					GroupVersion: "b/v1",
-					APIResources: []metav1.APIResource{
-						{
-							Name: "bar1",
-						},
-						{
-							Name: "bar2",
-						},
-					},
-				},
-			},
-		},
-		"legacy-policy-none-supported": {
-			existingAPIResources: []runtime.Object{
-				&metav1.APIResourceList{
-					GroupVersion: "a/v1",
-					APIResources: []metav1.APIResource{
-						{
-							Name: "foo1",
-						},
-						{
-							Name: "foo2",
-						},
-					},
-				},
-				&metav1.APIResourceList{
-					GroupVersion: "b/v1",
-					APIResources: []metav1.APIResource{
-						{
-							Name: "bar1",
-						},
-						{
-							Name: "bar2",
-						},
-					},
-				},
-			},
-			expectedErr: "the server does not support legacy policy resources",
-		},
-	}
-	discoverTests := map[string]struct {
-		existingAPIResources []runtime.Object
-		inputGVR             []schema.GroupVersionResource
-		expectedGVR          []schema.GroupVersionResource
-	}{
-		"regular": {
-			existingAPIResources: []runtime.Object{
-				&metav1.APIResourceList{
-					GroupVersion: "a/v1",
-					APIResources: []metav1.APIResource{
-						{
-							Name: "foo",
-						},
-					},
-				},
-				&metav1.APIResourceList{
-					GroupVersion: "b/v1",
-					APIResources: []metav1.APIResource{
-						{
-							Name: "bar",
-						},
-					},
-				},
-			},
-			inputGVR: []schema.GroupVersionResource{
-				{Group: "a", Version: "v1", Resource: "foo"},
-			},
-			expectedGVR: []schema.GroupVersionResource{
-				{Group: "a", Version: "v1", Resource: "foo"},
-			},
-		},
-	}
-
-	for tcName, tc := range legacyTests {
-		err := LegacyPolicyResourceGate(fake.NewSimpleClientset(tc.existingAPIResources...))
-		if err != nil && tc.expectedErr != err.Error() {
-			t.Fatalf("%s: expected err %s, got %s", tcName, tc.expectedErr, err.Error())
-		}
-	}
-
-	for tcName, tc := range discoverTests {
-		result, err := DiscoverGroupVersionResources(fake.NewSimpleClientset(tc.existingAPIResources...), tc.inputGVR)
-		if err != nil {
-			t.Fatalf("%s: unexpected err %s", tcName, err.Error())
-		}
-		if !reflect.DeepEqual(result, tc.expectedGVR) {
-			t.Fatalf("%s: expected %v, got %v", tcName, tc.expectedGVR, result)
-		}
-	}
-}
-
-/* Example stuff from discovery_client_test.go
-
-import (
 	"encoding/json"
-	"reflect"
-	"testing"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
+	"testing"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	//"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	//fake "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/fake"
-	"k8s.io/apimachinery/pkg/util/sets"
 	//"k8s.io/client-go/discovery"
+	"github.com/openshift/origin/pkg/authorization/apis/authorization"
+	"k8s.io/client-go/discovery"
 	restclient "k8s.io/client-go/rest"
-	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	//"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 )
 
 func TestGetServerResources(t *testing.T) {
-	stable := metav1.APIResourceList{
+	legacyAPIResourcesA := []metav1.APIResource{
+		{Name: "clusterpolicies", Kind: "ClusterPolicies"},
+		{Name: "clusterpolicybindings", Kind: "ClusterPolicyBindings"},
+		{Name: "policies", Kind: "Policies"},
+		{Name: "policybindings", Kind: "PolicyBindings"},
+		{Name: "foo", Kind: "Foo"},
+	}
+	legacyAPIResourcesB := []metav1.APIResource{
+		{Name: "clusterpolicies", Kind: "ClusterPolicies"},
+		{Name: "clusterpoliybindings", Kind: "ClusterPolicyBindings"},
+		{Name: "foo", Kind: "Foo"},
+	}
+	legacyAPIResourcesC := []metav1.APIResource{
+		{Name: "policies", Kind: "Policies"},
+		{Name: "policybindings", Kind: "PolicyBindings"},
+		{Name: "bar", Kind: "Bar"},
+	}
+	/*
+		legacyAPIResourcesD := []metav1.APIResource{
+			{Name: "foo", Kind: "Foo"},
+			{Name: "bar", Kind: "Bar"},
+			{Name: "baz", Kind: "Baz"},
+		}
+	*/
+	discoverListA := metav1.APIResourceList{
 		GroupVersion: "v1",
-		APIResources: []metav1.APIResource{
-			{Name: "pods", Namespaced: true, Kind: "Pod"},
-			{Name: "services", Namespaced: true, Kind: "Service"},
-			{Name: "namespaces", Namespaced: false, Kind: "Namespace"},
-		},
+		APIResources: legacyAPIResourcesA,
 	}
-	beta := metav1.APIResourceList{
-		GroupVersion: "extensions/v1beta1",
-		APIResources: []metav1.APIResource{
-			{Name: "deployments", Namespaced: true, Kind: "Deployment"},
-			{Name: "ingresses", Namespaced: true, Kind: "Ingress"},
-			{Name: "jobs", Namespaced: true, Kind: "Job"},
-		},
+	resourceListA := metav1.APIResourceList{
+		GroupVersion: authorization.LegacySchemeGroupVersion.String(),
+		APIResources: legacyAPIResourcesA,
 	}
-	tests := []struct {
+	resourceListB := metav1.APIResourceList{
+		GroupVersion: authorization.LegacySchemeGroupVersion.String(),
+		APIResources: legacyAPIResourcesB,
+	}
+	resourceListC := metav1.APIResourceList{
+		GroupVersion: authorization.LegacySchemeGroupVersion.String(),
+		APIResources: legacyAPIResourcesC,
+	}
+	/*
+		resourceListD := metav1.APIResourceList{
+			GroupVersion: authorization.LegacySchemeGroupVersion.String(),
+			APIResources: legacyAPIResourcesD,
+		}
+		resourceListE := metav1.APIResourceList{
+			GroupVersion: authorization.SchemeGroupVersion.String(),
+			APIResources: legacyAPIResourcesB,
+		}
+		resourceListF := metav1.APIResourceList{
+			GroupVersion: authorization.SchemeGroupVersion.String(),
+			APIResources: legacyAPIResourcesC,
+		}
+	*/
+	discoverTests := map[string]struct {
 		resourcesList *metav1.APIResourceList
+		inputGVR      []schema.GroupVersionResource
+		expectedGVR   []schema.GroupVersionResource
 		path          string
 		request       string
 		expectErr     bool
 	}{
-		{
-			resourcesList: &stable,
-			path:          "/api/v1",
-			request:       "v1",
-			expectErr:     false,
-		},
-		{
-			resourcesList: &beta,
-			path:          "/apis/extensions/v1beta1",
-			request:       "extensions/v1beta1",
-			expectErr:     false,
-		},
-		{
-			resourcesList: &stable,
-			path:          "/api/v1",
-			request:       "foobar",
-			expectErr:     true,
+		"discover": {
+			resourcesList: &resourceListA,
+			inputGVR: []schema.GroupVersionResource{
+				{
+					Group:    "",
+					Version:  "v1",
+					Resource: "foo",
+				},
+				{
+					Group:    "",
+					Version:  "v1",
+					Resource: "noexist",
+				},
+			},
+			expectedGVR: []schema.GroupVersionResource{
+				{
+					Group:    "",
+					Version:  "v1",
+					Resource: "foo",
+				},
+			},
+			path:      "/api/v1",
+			request:   "v1",
+			expectErr: false,
 		},
 	}
+	/*
+		discoverTests := map[string]struct {
+			resourcesList *metav1.APIResourceList
+			inputGVR      []schema.GroupVersionResource
+			path          string
+			request       string
+			expectErr     bool
+		}{
+			"disc-one": {
+				resourcesList: &resourceListA,
+				path:          "/api/" + authorization.LegacySchemeGroupVersion.String(),
+				request:       authorization.LegacySchemeGroupVersion.String(),
+				expectErr:     false,
+			},
+			"disc-two": {
+				resourcesList: &resourceListB,
+				path:          "/api/discover/v1",
+				request:       "discover/v1",
+				expectErr:     false,
+			},
+		}
+	*/
+	/*
+		legacyTests := map[string]struct {
+			resourcesList *metav1.APIResourceList
+			inputGVR      []schema.GroupVersionResource
+			path          string
+			request       string
+			expectErr     bool
+		}{
+			"legacy-one": {
+				resourcesList: &hasLegacyResources,
+				path:          "/api/v1",
+				request:       "v1",
+				expectErr:     false,
+			},
+			"legacy-two": {
+				resourcesList: &testDiscoveryResourcesV1,
+				path:          "/api/discover/v1",
+				request:       "discover/v1",
+				expectErr:     false,
+			},
+		}
+	*/
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		var list interface{}
 		switch req.URL.Path {
 		case "/api/v1":
-			list = &stable
-		case "/apis/extensions/v1beta1":
-			list = &beta
-		case "/api":
-			list = &metav1.APIVersions{
-				Versions: []string{
-					"v1",
-				},
-			}
-		case "/apis":
-			list = &metav1.APIGroupList{
-				Groups: []metav1.APIGroup{
-					{
-						Versions: []metav1.GroupVersionForDiscovery{
-							{GroupVersion: "extensions/v1beta1"},
-						},
-					},
-				},
-			}
+			list = &discoverListA
+		case "/api/policy/A":
+			list = &resourceListA
+		case "/api/policy/B":
+			list = &resourceListB
+		case "/api/policy/C":
+			list = &resourceListC
 		default:
 			t.Logf("unexpected request: %s", req.URL.Path)
 			w.WriteHeader(http.StatusNotFound)
@@ -233,49 +177,24 @@ func TestGetServerResources(t *testing.T) {
 		w.Write(output)
 	}))
 	defer server.Close()
-	client := internalclientset.NewForConfigOrDie(&restclient.Config{Host: server.URL})
-	//client := discovery.NewDiscoveryClientForConfigOrDie(&restclient.Config{Host: server.URL})
+	client := discovery.NewDiscoveryClientForConfigOrDie(&restclient.Config{Host: server.URL})
 
-	got, err := DiscoverGroupVersionResources(client, []schema.GroupVersionResource{{Version: "v1", Resource: "pods"}})
-	if err != nil {
-		t.Fatalf("myerr %s", err.Error())
-	}
-	t.Errorf("%v", got)
-	for _, test := range tests {
-		got, err := client.ServerResourcesForGroupVersion(test.request)
-		if test.expectErr {
-			if err == nil {
-				t.Error("unexpected non-error")
+	/*
+		for _, tc := range legacyTests {
+			err := LegacyPolicyResourceGate(client)
+			if err != nil {
+				t.Fatalf("legacy err")
 			}
-			continue
 		}
+	*/
+	for tcName, tc := range discoverTests {
+		got, err := DiscoverGroupVersionResources(client, tc.inputGVR...)
 		if err != nil {
-			t.Errorf("unexpected error: %v", err)
-			continue
+			t.Fatalf("myerr %s", err.Error())
 		}
-		if !reflect.DeepEqual(got, test.resourcesList) {
-			t.Errorf("expected:\n%v\ngot:\n%v\n", test.resourcesList, got)
+		if !reflect.DeepEqual(got, tc.expectedGVR) {
+			t.Errorf("%s got %v, expected %v", tcName, got, tc.inputGVR)
 		}
 	}
 
-	serverResources, err := client.ServerResources()
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-
-	serverGroupVersions := sets.NewString(groupVersions(serverResources)...)
-	for _, api := range []string{"v1", "extensions/v1beta1"} {
-		if !serverGroupVersions.Has(api) {
-			t.Errorf("missing expected api %q in %v", api, serverResources)
-		}
-	}
 }
-
-func groupVersions(resources []*metav1.APIResourceList) []string {
-	result := []string{}
-	for _, resourceList := range resources {
-		result = append(result, resourceList.GroupVersion)
-	}
-	return result
-}
-*/
